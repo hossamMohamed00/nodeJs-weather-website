83f7d4da3e26ad3f3ddb04b8ce6902ce98791cfd
hossammohamed00 <a01122882174@gmail.com>
2020-09-29 20:48:51 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ const getForecast = (address) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     
-    fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+    fetch('/weather?address=' + address).then((response) => {
     response.json().then((data) => {
         if (data.error) {
              messageOne.textContent = data.error;
